c9cebb3224391d95ea87523387e7f38251c4cfd6
AngryPacifist <meldiablo23@gmail.com>
2022-04-19 13:19:40 +0100

    Header file

---
+++ main.h
@@ -0,0 +1,27 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdarg.h>
+/**
+ * struct find - function object
+ * @ch: flag
+ * @function: function
+ */
+
+ typedef struct find
+ {
+     char *ch;
+     int (*function)(va_list);
+ }fin_t;
+
+ int _printf(const char *format, ...);
+ int (*find_funct(const char *s, int p))(va_list);
+ int _putchar(char c);
+ int print_char(va_list c);
+ int print_string(va_list s);
+ int print_decimal(va_list d);
+
+#endif // MAIN_H
